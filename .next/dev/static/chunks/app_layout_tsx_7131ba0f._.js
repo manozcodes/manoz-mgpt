3f0db881299c8c8e819074f45452a70eaacf1de6
manozcodes <manozacharya1998@gmail.com>
2025-12-26 23:01:12 +0545

    readme.md

--- .next/dev/static/chunks/app_layout_tsx_7131ba0f._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__ba9e3890._.css"
-],
-    source: "dynamic"
-});
